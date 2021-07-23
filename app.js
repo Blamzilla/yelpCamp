bfa743b3b4229f9d0d7fd4ef65d5dbdaee727975
Cory Leclair <cory.leclair@gmail.com>
2021-07-23 00:42:08 -0400

    mongoose wired up

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const path = require("path");
 const uuid = require("uuid");
 const methodOverride = require("method-override");
 
+//DB connection section
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console.error, "connection error: "));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
 const app = express();
 
 //middleware section
