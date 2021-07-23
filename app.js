08d7d705779ee81025a70a5cd7c89bb44cb36917
Cory Leclair <cory.leclair@gmail.com>
2021-07-23 14:47:33 -0400

    erger

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.put("/campgrounds/:id", async (req, res) => {
 
 app.delete("/campgrounds/:id", async (req, res) => {
   const { id } = req.body;
-  console.log(req.body);
+  console.log(req.params);
   const findCamp = await Campground.findByIdAndDelete(id);
   res.redirect(`/campgrounds`);
 });
