2ab35ab8736a6e353d550fe53ccbae5a637eaedf
Cory Leclair <cory.leclair@gmail.com>
2021-07-23 00:27:01 -0400

    ere

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("root");
+});
+
 app.listen(3000, () => {
   console.log("listening on 3000");
 });
