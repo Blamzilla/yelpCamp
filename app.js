5702c7867e6fe3e32ee6ef56e74c759419805525
Cory Leclair <cory.leclair@gmail.com>
2021-07-23 14:05:34 -0400

    werfew

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 app.post("/campgrounds", async (req, res) => {
-  res.send(req.body.campground);
+  res.send(req.body);
   //await newCamp.save();
   //res.redirect("/campgrounds");
 });
