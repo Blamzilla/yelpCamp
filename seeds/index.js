567527186c951600261aa96d77954ce3e07c486f
Cory Leclair <cory.leclair@gmail.com>
2021-07-23 23:00:55 -0400

    styles and formatting

--- seeds/index.js
+++ seeds/index.js
@@ -20,10 +20,17 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
+    const collection = ["483251", "24195127", "19655117"];
+    const randomCollection = Math.floor(Math.random() * 3);
+    const price = Math.floor(Math.random() * 20) + 10;
     const random1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://source.unsplash.com/collection/${collection[randomCollection]}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ratione quisquam, molestias numquam earum eveniet accusantium totam omnis magnam dolore cum aspernatur, non quae sed qui libero, ea voluptatem minus.",
+      price: price,
     });
     await camp.save();
   }
